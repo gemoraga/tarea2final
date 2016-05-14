1555684dbd4fd41ea154479c936d8f34961c2a1d
gemoraga <gemoraga@uc.cl>
2016-05-14 14:01:19 -0400

    cambio de test

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -4,8 +4,8 @@ class ApiControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "get index" do
-    get :index
+  test "buscar funciona" do
+    post :buscar, tag: 'futbol', access_token:'2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402' 
     assert_response :success
   end
 end
