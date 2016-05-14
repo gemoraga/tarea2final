cc6d25fe75f85a2d588d46ba854cdf2355455a31
gemoraga <gemoraga@uc.cl>
2016-05-14 14:11:47 -0400

    faltaba un end

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -11,4 +11,5 @@ class ApiControllerTest < ActionController::TestCase
   test "buscar exige parámetro tag" do
   post :buscar, access_token:'2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402'
   assert_response 400
+  end
 end
