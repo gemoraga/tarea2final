d8f3724541456cea993fe189741693910e5a92a6
gemoraga <gemoraga@uc.cl>
2016-05-14 00:16:10 -0400

    agregada funcionalidad para devolver posts

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -10,12 +10,36 @@ require 'rest-client'
     respuestaParseada = JSON.parse respuesta
     datos = respuestaParseada["data"]
     cantidadPosts = datos["media_count"]
-    puts datos
-    puts cantidadPosts
+    temp = RestClient.get 'https://api.instagram.com/v1/tags/' + tag.to_s + '/media/recent/', {:content_type => 'application/json', :params => {:count => 20, :access_token => access_token}}
+    tempParseado = JSON.parse temp
+    fotos = tempParseado["data"]
+    #tags = datosFotos["tags"]
+    #username = datosFotos["user"]["username"]
+    #likes = datosFotos["likes"]["count"]
+    #url = datosFotos["images"]["standard_resolution"]
+    #caption = datosFotos["caption"]["text"]
+    i = 0
+    while i < fotos.length
+      datosFotos = fotos[i]
+      tags = datosFotos["tags"]
+      username = datosFotos["user"]["username"]
+      likes = datosFotos["likes"]["count"]
+      url = datosFotos["images"]["standard_resolution"]["url"]
+      caption = datosFotos["caption"]["text"]
+      if i == 0 
+        posts = '{tags: ' + tags.to_s + ', username: ' + username.to_s + ', likes: ' + likes.to_s + ', url: ' + url.to_s + ', caption: ' + caption.to_s + '}'
+      else
+        post = ', {tags: ' + tags.to_s + ', username: ' + username.to_s + ', likes: ' + likes.to_s + ', url: ' + url.to_s + ', caption: ' + caption.to_s + '}'
+        posts += post
+      end
+    posts2 = '{' + posts + '}'
+    i += 1
+    end
+    puts posts2
     render :status => status, json: {
       metadata: {total: cantidadPosts},
-      posts: 'chao',
-      version: '1.1.1'
+      posts: posts2,
+      version: '1.2.0'
     }
   end
 end
