f8c9e04ff0ca864e2e79561e4fd6c20c7e8bc5a1
gemoraga <gemoraga@uc.cl>
2016-05-15 16:27:17 -0400

    arreglado error de formato, version 1.3.1 de la api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,7 +4,7 @@ require 'json'
 require 'rest-client'
   def buscar
     status = 200
-    begin
+    #begin
     tag = params.require(:tag)
     access_token = params.require(:access_token)
     respuesta = RestClient.get 'https://api.instagram.com/v1/tags/' + tag.to_s + '?access_token=' + access_token.to_s, :content_type => 'application/json'
@@ -15,30 +15,39 @@ require 'rest-client'
     tempParseado = JSON.parse temp
     fotos = tempParseado["data"]
     i = 0
+    posts2 = []
     while i < fotos.length
       datosFotos = fotos[i]
       tags = datosFotos["tags"]
       username = datosFotos["user"]["username"]
       likes = datosFotos["likes"]["count"]
       url = datosFotos["images"]["standard_resolution"]["url"]
+      begin
       caption = datosFotos["caption"]["text"]
-      if i == 0 
-        posts = '{tags: ' + tags.to_s + ', username: ' + username.to_s + ', likes: ' + likes.to_s + ', url: ' + url.to_s + ', caption: ' + caption.to_s + '}'
-      else
-        post = ', {tags: ' + tags.to_s + ', username: ' + username.to_s + ', likes: ' + likes.to_s + ', url: ' + url.to_s + ', caption: ' + caption.to_s + '}'
-        posts += post
+      rescue
+      caption = '#' + tag
       end
-    posts2 = '{' + posts + '}'
+      if i == 0 
+        post = ({tags: tags, username: username, likes: likes, url: url, caption: caption})
+        #JSON.parse(post)
+      else
+        post = ({tags: tags, username: username, likes: likes, url: url, caption: caption})
+        #JSON.parse(post)
+        #post = ('{tags: ' + tags.to_s + ', username: ' + username.to_s + ', likes: ' + likes.to_s + ', url: ' + url.to_s + ', caption: ' + caption.to_s + '}').to_json
+        #posts += post
+        posts2 << post
+      end
+    #posts2 = '[' + posts + ']'
     i += 1
     end
-    puts posts2
+    #puts posts2
     render :status => status, json: {
       metadata: {total: cantidadPosts},
       posts: posts2,
-      version: '1.3.0'
+      version: '1.3.1'
     }
-  rescue
-  render :status => 400
-  end
+  #rescue
+  #render :status => 400
+  #end
   end
 end
